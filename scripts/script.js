a97d550263ac3b262b393f8cea3f06e052296f91
asmaaHamdym <asmahamdym@gmail.com>
2024-05-11 01:01:14 +0300

    added functionality for the add task button

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-tasks = [];
+let tasks = [];
+
+let addTask = () => {
+  if (input.value) {
+    input.value = "";
+    tasks.push(input.value);
+    console.log(tasks);
+  }
+};
+
+const input = document.querySelector("input");
+const add_task_button = document.getElementById("add_task");
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+  add_task_button.removeAttribute("disabled");
+});
+add_task_button.addEventListener("click", addTask);
