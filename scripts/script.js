387648a180b66e331979a0bac365c97bef13a588
asmaaHamdym <asmahamdym@gmail.com>
2024-05-11 01:19:23 +0300

    Added id attributes to  DOM elements for todo list display and add render function for tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
+const input = document.querySelector("input");
+const add_task_button = document.getElementById("add_task");
+const todo_list = document.getElementById("todos");
+
 let tasks = [];
 
 let addTask = () => {
   if (input.value) {
     input.value = "";
     tasks.push(input.value);
-    console.log(tasks);
+    renderTasks();
   }
 };
 
-const input = document.querySelector("input");
-const add_task_button = document.getElementById("add_task");
+let renderTasks = () => {};
 
 input.addEventListener("input", () => {
   console.log(input.value);
